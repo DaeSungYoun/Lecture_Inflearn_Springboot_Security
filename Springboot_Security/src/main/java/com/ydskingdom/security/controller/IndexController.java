6aa5dcde3a411bfc6e8e0c7b01c7c9a6a44c50ca
DaeSungYoun <ydskingdom@gmail.com>
2023-02-20 22:15:48 +0900

    스프링부트 시큐리티 8강 - Authentication객체가 가질 수 있는 2가지 타입

--- Springboot_Security/src/main/java/com/ydskingdom/security/controller/IndexController.java
+++ Springboot_Security/src/main/java/com/ydskingdom/security/controller/IndexController.java
@@ -1,11 +1,16 @@
 package com.ydskingdom.security.controller;
 
+import com.ydskingdom.security.config.auth.PrincipalDetails;
 import com.ydskingdom.security.model.User;
 import com.ydskingdom.security.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +22,27 @@ public class IndexController {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @GetMapping("/test/login")
+    public @ResponseBody String testLogin(Authentication authentication, @AuthenticationPrincipal PrincipalDetails userDetails){
+        System.out.println("=====================test login");
+        PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+        System.out.println("principalDetails : " + principalDetails.getUser());
+
+        System.out.println("userDetails : " + userDetails);
+        System.out.println("userDetails : " + userDetails.getUser());
+        return "세션 정보 확인하기";
+    }
+
+    @GetMapping("/test/oauth/login")
+    public @ResponseBody String testOAuthLogin(Authentication authentication, @AuthenticationPrincipal OAuth2User oAuth2){
+        System.out.println("=====================test login");
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        System.out.println("authentication : " + oAuth2User.getAttributes());
+        System.out.println("oAuth2User : " + oAuth2.getAttributes());
+
+        return "OAuth 정보 확인하기";
+    }
 
     @GetMapping({"", "/"})
     public @ResponseBody
